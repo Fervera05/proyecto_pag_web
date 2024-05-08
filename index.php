4460539ba335d5e6c59d32839277e75357854542
Fervera05 <fervera455@gmail.com>
2024-05-08 13:50:34 -0400

    Se agrega el archivo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <Nav>
+            <ul>
+                 <li>  <div class="search-container">
+                    <form action="/buscar" method="post">
+                      <input type="text" placeholder="Buscar..." name="q" class="search-box">
+                      <button type="submit" class="search-button">Buscar</button>
+                    </form>
+                  </div></li>
+                <li class="nav-itemo"></li>
+               <a href=""><li class="nav-item">Inicio <hr></li></a> 
+                <li class="nav-item">Productos <hr></li>
+                <li class="nav-item">Más información <hr></li>
+            </ul>
+        </Nav>
+    </header>
+    <br> <br> <br> <br>
+        <div class="container ">
+
+        </div>
+</body>
+</html>
